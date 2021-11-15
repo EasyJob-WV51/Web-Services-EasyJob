71f68fce517ba4b604a2e07f0d4d9496a614c12f
SebastianBran <sebastianbranzapata@gmail.com>
2021-11-14 22:56:29 -0500

    feat: dtos for search applicant
    I made the dtos for the search applicant

---
+++ src/applicants/application/dtos/queries/search-applicant.dto.ts
@@ -0,0 +1,11 @@
+export class SearchApplicantDto {
+  public id: number;
+  public firstName: string;
+  public lastName: string;
+  public email: string;
+  public mySpecialty: string;
+  public myExperience: string;
+  public description: string;
+  public nameGithub: string;
+  public imgApplicant: string;
+}
